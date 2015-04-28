76725066d0cbe8a7a9d17fb947e3d2f8ae0660fa
MikeDMorgan <michael.morgan@dpag.ox.ac.uk>
2015-04-28 14:39:29 +0100

    fix PipelineTimeseries import statement

--- CGATPipelines/pipeline_timeseries.py
+++ CGATPipelines/pipeline_timeseries.py
@@ -165,7 +165,7 @@ import CGAT.Experiment as E
 import CGAT.IOTools as IOTools
 import CGAT.Database as Database
 import CGAT.GTF as GTF
-import CGATPipelines.PipelineTimeseries as PipelineTimeseries
+import CGAT.Timeseries as Timeseries
 import CGATPipelines.PipelineTracks as PipelineTracks
 
 ###################################################
@@ -756,7 +756,7 @@ if ANALYSIS == 'replicates':
         '''
 
         outdir = outfile.split("/")[0]
-        PipelineTimeseries.splitReplicates(infile=infile,
+        Timeseries.splitReplicates(infile=infile,
                                            axis="column",
                                            group_var="replicates",
                                            outdir=outdir)
@@ -777,7 +777,7 @@ if ANALYSIS == 'replicates':
             Arbitrarily split files into chunks for parallelisation
             '''
 
-            PipelineTimeseries.splitFiles(infile=infile,
+            Timeseries.splitFiles(infile=infile,
                                           nchunks=PARAMS['resampling_chunks'],
                                           out_dir="parallel_files.dir")
             P.touch(outfile)
@@ -1047,7 +1047,7 @@ elif PARAMS["resampling_analysis_type"] == 'resample':
             Arbitrarily split files into chunks for parallelisation
             '''
 
-            PipelineTimeseries.splitFiles(infile=infile,
+            Timeseries.splitFiles(infile=infile,
                                           nchunks=PARAMS['resampling_chunks'],
                                           out_dir="parallel_files.dir")
             P.touch(outfile)
