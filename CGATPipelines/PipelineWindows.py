e2a5b1b697acacb5cda6997fb96a3410bcf3a8b4
Andreas Heger <andreas.heger@gmail.com>
2015-09-30 10:18:16 +0100

    sort intervals by end and name for defined sort order

--- CGATPipelines/PipelineWindows.py
+++ CGATPipelines/PipelineWindows.py
@@ -211,7 +211,7 @@ def countTagsWithinWindows(tagfile,
     zcat %(tagfile)s
     %(f)s
     | coverageBed -a stdin -b %(windowfile)s -split
-    | sort -k1,1 -k2,2n -k4,4
+    | sort -k1,1 -k2,2n -k3,3n -k4,4
     | gzip
     > %(outfile)s
     '''
