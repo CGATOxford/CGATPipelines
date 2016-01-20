02ade0a25e15dc7f847b530eb7a420a7f7de3f4b
Acribbs <adam.cribbs@dpag.ox.ac.uk>
2016-01-20 15:29:01 +0000

    initial tracker to pull out all data to work on using ipython

--- CGATPipelines/pipeline_docs/pipeline_rnaseqqc/trackers/RnaseqqcReport.py
+++ CGATPipelines/pipeline_docs/pipeline_rnaseqqc/trackers/RnaseqqcReport.py
@@ -156,6 +156,19 @@ class sampleMDS(RnaseqqcTracker):
 
         return pos
 
+class sampleOverlaps(RnaseqqcTracker):
+    table = "transcript_quantification"
+
+    def __call__(self, track, slice=None):
+
+        statement = (
+            "SELECT transcript_id, TPM, sample_id FROM %(table)s "
+            "WHERE transcript_id != 'Transcript'")
+
+        df = pd.DataFrame.from_dict(self.getAll(statment))
+
+        return df
+
 
 # TS: Correlation trackers should be simplified and use tracks to
 # select subsets
