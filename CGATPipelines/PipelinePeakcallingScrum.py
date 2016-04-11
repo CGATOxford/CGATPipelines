111af9f753055ffc9b6002d1337c4837fb21ff6a
katherineb <katherine.brown@dpag.ox.ac.uk>
2016-04-11 11:25:12 +0100

    initial blank pipeline

---
+++ CGATPipelines/PipelinePeakcallingScrum.py
@@ -0,0 +1,22 @@
+'''
+======================================================================
+
+Requirements:
+
+
+Reference
+---------
+
+'''
+
+import os
+import re
+import collections
+import pandas
+import math
+import numpy
+import numpy.ma as ma
+import itertools
+import CGAT.Experiment as E
+import CGATPipelines.Pipeline as P
+import CGAT.IOTools as IOTools
---
+++ CGATPipelines/pipeline_peakcallingScrum.py
@@ -0,0 +1,126 @@
+"""
+pipeline_peakcalling.py - Window based genomic analysis
+===================================================
+
+:Author: Andreas Heger
+:Release: $Id$
+:Date: |today|
+:Tags: Python
+
+
+Methods
+=======
+
+Usage
+=====
+
+See :ref:`PipelineSettingUp` and :ref:`PipelineRunning` on general
+information how to use CGAT pipelines.
+
+Configuration
+-------------
+
+Input
+-----
+
+
+Pipeline output
+===============
+
+
+Code
+====
+
+"""
+
+# load modules
+from ruffus import transform, merge, mkdir, follows, \
+    regex, suffix, add_inputs, collate
+
+import sys
+import os
+import re
+import csv
+import numpy
+import sqlite3
+import pandas
+import glob
+import CGAT.Experiment as E
+import CGAT.IOTools as IOTools
+import CGATPipelines.Pipeline as P
+
+
+#########################################################################
+#########################################################################
+#########################################################################
+# load options from the config file
+P.getParameters(
+    ["%s/pipeline.ini" % os.path.splitext(__file__)[0],
+     "../pipeline.ini",
+     "pipeline.ini"],
+    defaults={
+        'paired_end': False})
+
+PARAMS = P.PARAMS
+
+PARAMS.update(P.peekParameters(
+    PARAMS["annotations_dir"],
+    "pipeline_annotations.py",
+    prefix="annotations_",
+    update_interface=True))
+
+
+###################################################################
+# Helper functions mapping tracks to conditions, etc
+###################################################################
+# load all tracks - exclude input/control tracks
+
+def connect():
+    '''connect to database.
+
+    This method also attaches to helper databases.
+    '''
+
+    dbh = sqlite3.connect(PARAMS["database_name"])
+    statement = '''ATTACH DATABASE '%s' as annotations''' %\
+                (PARAMS["annotations_database"])
+    cc = dbh.cursor()
+    cc.execute(statement)
+    cc.close()
+
+    return dbh
+
+
+def full():
+    pass
+
+
+@follows(mkdir("report"))
+def build_report():
+    '''build report from scratch.'''
+
+    E.info("starting documentation build process from scratch")
+    P.run_report(clean=True)
+
+
+@follows(mkdir("report"))
+def update_report():
+    '''update report.'''
+
+    E.info("updating documentation")
+    P.run_report(clean=False)
+
+
+@follows(mkdir("%s/bamfiles" % PARAMS["web_dir"]),
+         mkdir("%s/medips" % PARAMS["web_dir"]),
+         )
+def publish():
+    '''publish files.'''
+
+    # directory : files
+
+    # publish web pages
+    P.publish_report(export_files=export_files)
+
+if __name__ == "__main__":
+    sys.exit(P.main(sys.argv))
