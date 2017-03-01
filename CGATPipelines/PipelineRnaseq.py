cef02eadd187dda6374b0d0f1785fdeef788830f
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2017-03-01 16:43:11 +0000

    syntax change in PipelineRnaseq.py

--- CGATPipelines/PipelineRnaseq.py
+++ CGATPipelines/PipelineRnaseq.py
@@ -1886,7 +1886,7 @@ def buildUTRExtension(infile, outfile):
             states = None
             try:
                 states = list(R('''states = Viterbi( hmm )'''))
-            except ri.RRuntimeError, msg:
+            except ri.RRuntimeError as msg:
                 counter.skipped_error += 1
                 new_utrs[gene_id] = Utr._make((old_utr, None, None, "fail"))
                 continue
