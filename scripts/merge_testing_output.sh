b9ef19395efdf508e9841cefc23ea974b649ee93
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2017-03-15 17:06:17 +0000

    add new script to help summarise testing output

---
+++ scripts/merge_testing_output.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Function to print pretty output
+pprint() {
+
+   printf "%s\t%s\t%s\n" $1 $2 $3
+}
+
+# sanity check; we want input files
+if [ $# -eq 0 ] ; then
+
+   echo
+   echo " ERROR: no input files given "
+   echo " USAGE: merge_testing_output.sh test_mapping.exist test_mapping.lines test_mapping.md5"
+   echo
+   exit 1;
+
+fi
+
+# check which input files we have
+md5file=`echo $@ | sed 's/\s/\n/g' | grep md5`
+linesfile=`echo $@ | sed 's/\s/\n/g' | grep lines`
+existfile=`echo $@ | sed 's/\s/\n/g' | grep exist`
+
+# check whether they can be read
+[ -z "$md5file" ] || [ ! -r $md5file ] || [ ! -s $md5file ] && md5check=1
+[ -z "$linesfile" ] || [ ! -r $linesfile ] || [ ! -s $linesfile ] && linescheck=1
+[ -z "$existfile" ] || [ ! -r $existfile ] || [ ! -s $existfile ] && existcheck=1
+
+if [ $md5check -a $linescheck -a $existcheck ] ; then
+
+   echo
+   echo " ERROR: one or more input files do not exist, are empty or cannot be read"
+   echo " Input files were: " $@
+   echo
+   exit 1;
+
+fi
+
+# merge all file names
+awk '{print $1;}' $md5file $linesfile $existfile | sort -u > temp
+
+pprint "filename" "nlines" "md5"
+
+for file in `cat temp` ; do
+
+   awkfile=$(echo $file | sed 's/\//\\\//g')
+   [ -n "$md5file"   ] && md5=`awk '/'$awkfile'/ { print $2; }' $md5file`
+   [ -n "$linesfile" ] && nlines=`awk '/'$awkfile'/ { print $2; }' $linesfile`
+
+   [ -z "$nlines" ] && [ -z "$md5" ] && pprint $file "-1" "-1"
+   [ -z "$nlines" ] && [ -n "$md5" ] && pprint $file "-1" $md5
+   [ -n "$nlines" ] && [ -n "$md5" ] && pprint $file $nlines $md5
+   [ -n "$nlines" ] && [ -z "$md5" ] && pprint $file $nlines "-1"
+
+done
+
+# cleanup temp file
+rm -f temp
+
