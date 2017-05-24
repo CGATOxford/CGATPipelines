5ee8552ee6fa40950d98c1fd27a573f82a5d67bd
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2017-05-24 17:56:13 +0100

    refactor script

--- scripts/cgat_check_deps.py
+++ scripts/cgat_check_deps.py
@@ -12,7 +12,7 @@ located on the PATH.
 Usage
 -----
 
-.. cgat cgat_check_deps --pipeline </path/to/pipeline_name.py>
+.. cgat cgat_check_deps --pipeline </path/to/pipeline_name.py> [--print-summary]
 
 Example::
 
@@ -46,30 +46,11 @@ P.getParameters(
 PARAMS = P.PARAMS
 
 
-def main(argv=None):
-    """script main.
-    parses command line options in sys.argv, unless *argv* is given.
-    """
-
-    if argv is None:
-        argv = sys.argv
-
-    # setup command line parser
-    parser = E.OptionParser(version="%prog version: $Id$",
-                            usage=globals()["__doc__"])
-
-    parser.add_option("-p", "--pipeline", dest="pipeline", type="string",
-                      help="Path to pipeline script")
-
-    parser.add_option("-s", "--print-summary", dest="summary", action="store_true", default=False,
-                      help="Print how many times a program is used [default=%default]")
-
-    # add common options (-h/--help, ...) and parse command line
-    (options, args) = E.Start(parser, argv=argv)
+def checkDepedencies(pipeline):
 
     # check existence of pipeline script
-    if not os.access(options.pipeline, os.R_OK):
-        raise IOError("Pipeline %s was not found\n" % options.pipeline)
+    if not os.access(pipeline, os.R_OK):
+        raise IOError("Pipeline %s was not found\n" % pipeline)
 
     # parse pipeline script
     statement = '''awk '/statement =/,/P.run()/ {print}' %s
@@ -86,7 +67,7 @@ def main(argv=None):
     if statement.endswith(";"):
         statement = statement[:-1]
 
-    process = subprocess.Popen(statement % options.pipeline,
+    process = subprocess.Popen(statement % pipeline,
                                shell=True,
                                stdin=subprocess.PIPE,
                                stdout=subprocess.PIPE,
@@ -102,7 +83,8 @@ def main(argv=None):
             "The stderr was: \n%s\n%s\n" %
              (-process.returncode, stderr, statement))
 
-    print stdout
+    # check awk command output
+    #print stdout
 
     # dictionary where:
     # key = program name
@@ -126,23 +108,54 @@ def main(argv=None):
                     else:
                         deps[prog_name]+= 1
 
+    # list of unmet dependencies
     check_path_failures = []
 
     # print dictionary ordered by value
     for k in sorted(deps, key=deps.get, reverse=True):
-        if options.summary:
-            print 'Program: {0!s} used {1} time(s) \n'.format(k,deps[k])
-        if len(IOTools.which(k)) == 0:
+        if IOTools.which(k) is None:
             check_path_failures.append(k)
-            print 'Program: {0!s} is NOT on your PATH!\n'.format(k)
+
+    return deps, check_path_failures
+
+
+def main(argv=None):
+    """script main.
+    parses command line options in sys.argv, unless *argv* is given.
+    """
+
+    if argv is None:
+        argv = sys.argv
+
+    # setup command line parser
+    parser = E.OptionParser(version="%prog version: $Id$",
+                            usage=globals()["__doc__"])
+
+    parser.add_option("-p", "--pipeline", dest="pipeline", type="string",
+                      help="Path to pipeline script")
+
+    parser.add_option("-s", "--print-summary", dest="summary", action="store_true", default=False,
+                      help="Print how many times a program is used [default=%default]")
+
+    # add common options (-h/--help, ...) and parse command line
+    (options, args) = E.Start(parser, argv=argv)
+
+    # check pipeline dependencies
+    deps, check_path_failures = checkDepedencies(options.pipeline)
+
+    # print dictionary ordered by value
+    if options.summary:
+        for k in sorted(deps, key=deps.get, reverse=True):
+            print('\nProgram: {0!s} used {1} time(s)'.format(k,deps[k]))
 
     n_failures = len(check_path_failures)
     if n_failures == 0:
-        print 'Congratulations! All required programs are available on your PATH\n'
+        print('\nCongratulations! All required programs are available on your PATH\n')
     else:
-        print 'The following programs are not on your PATH\n'
-        for p in check_path_failure:
-            print '{0!s}\n'.format(p)
+        print('\nThe following programs are not on your PATH')
+        for p in check_path_failures:
+            print('\n{0!s}'.format(p))
+        print
 
     # write footer and output benchmark information.
     E.Stop()
