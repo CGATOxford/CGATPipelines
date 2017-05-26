7ab5093f3a52a9d2e5f90a957e220d52447a6878
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2017-05-26 09:53:02 +0100

    replace awk command with Python's ast module

--- scripts/cgat_check_deps.py
+++ scripts/cgat_check_deps.py
@@ -5,18 +5,28 @@ cgat_check_deps.py - check whether the software dependencies are on your PATH
 Purpose
 -------
 
-.. This script takes the name of a CGAT pipeline and checks whether all
-programs used in the pipeline (via "statement" and "P.run()") can be
-located on the PATH.
+.. The goal of this script is to provide a list of third-party command-line
+programs used in a Python script given as input, and check whether
+they are on your PATH. This is useful to identify dependencies across all
+CGAT pipelines and module files.
+
+This script takes the path to a Python script, which is expected to call
+command-line programs like we do in CGAT pipelines, i.e.:
+
+   statement = """cmd-1 | cmd-2 | cmd-3""""
+   P.run()
+
+Programs called other way (e.g. using subprocess) will not be picked up
+by this script.
 
 Usage
 -----
 
-.. cgat cgat_check_deps --pipeline </path/to/pipeline_name.py> [--print-summary]
+.. python cgat_check_deps --pipeline </path/to/pipeline_name.py> [--print-summary]
 
 Example::
 
-   cgat cgat_check_deps --pipeline CGATPipelines/pipeline_annotations.py
+   python cgat_check_deps --pipeline CGATPipelines/pipeline_annotations.py
 
 Type::
 
@@ -32,18 +42,10 @@ Command line options
 import os
 import sys
 import re
+import ast
 import subprocess
 import CGAT.Experiment as E
 import CGAT.IOTools as IOTools
-
-# load options from the config file
-import CGATPipelines.Pipeline as P
-P.getParameters(
-    ["%s/pipeline.ini" % os.path.splitext(__file__)[0],
-     "../pipeline.ini",
-     "pipeline.ini"])
-
-PARAMS = P.PARAMS
 
 
 def checkDepedencies(pipeline):
@@ -56,60 +58,56 @@ def checkDepedencies(pipeline):
         raise IOError("The given input is a folder, and must be a script\n")
 
     # parse pipeline script
-    statement = '''awk '/statement =/,/P.run()/ {print}' %s
-                   | grep -v '#'
-                   | tr '\n' ' '
-                   | tr '"' ' '
-                   | sed 's/statement =//g'
-                   | sed 's/P.run()//g'
-                   '''
-    # the code below has been adapted from the Execution module of Pipeline.py
-    # cleaning up of statement
-    # remove new lines and superfluous spaces and tabs
-    statement = " ".join(re.sub("\t+", " ", statement).split("\n")).strip()
-    if statement.endswith(";"):
-        statement = statement[:-1]
+    with open(pipeline) as f:
+        tree = ast.parse(f.read())
 
-    process = subprocess.Popen(statement % pipeline,
-                               shell=True,
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+    # list to store all statements = ''' <commands> '''
+    statements = []
 
-    # get process output
-    stdout, stderr = process.communicate()
+    # inspired by
+    # https://docs.python.org/3/library/ast.html#module-ast
+    # http://bit.ly/2rDf5xu
+    # http://bit.ly/2r0Uv9t
+    for node in ast.walk(tree):
+        if type(node) is ast.Assign and \
+           hasattr(node, 'targets') and \
+           hasattr(node.targets[0], 'id') and \
+           node.targets[0].id == "statement" and \
+           hasattr(node.value, 's'):
+#        if hasattr(node, 'targets') and hasattr(node.targets[0], 'id') and node.targets[0].id == "statement":
+             statement = node.value.s
+             # clean up statement, code copied from Execution module of Pipeline.py
+             statement = " ".join(re.sub("\t+", " ", statement).split("\n")).strip()
+             if statement.endswith(";"):
+                 statement = statement[:-1]
+             statements.append(statement)
 
-    # check process status
-    if process.returncode != 0:
-        raise OSError(
-            "Child was terminated by signal %i: \n"
-            "The stderr was: \n%s\n%s\n" %
-            (-process.returncode, stderr, statement))
-
-    # check awk command output
-    if len(stdout) == 0:
-        raise Exception("The given input does not contain P.run() statements.")
-
-    # check awk command output
-    #print stdout
- 
     # dictionary where:
     # key = program name
     # value = number of times it has been called
     deps = {}
 
-    for slice in stdout.split("'''"):
-        # clean up white duplicated spaces
-        clean_slice = ' '.join(slice.split())
-        if len(clean_slice) > 0:
-            for command in clean_slice.split("|"):
-                # take program name
-                groups = re.match("^\s*(\w+)", command)
-                if groups is not None:
-                    # program name is first match
-                    prog_name = groups.group(0)
-                    # clean up duplicated white spaces
-                    prog_name = ' '.join(prog_name.split())
+    # set of names that are not proper deps
+    exceptions = ['create',
+                  'drop',
+                  'select',
+                  'attach',
+                  'insert',
+                  'module',
+                  'for']
+
+    for statement in statements:
+        for command in statement.split("|"):
+            print "COMMAND: ", command
+            # take program name
+            groups = re.match("^\s*(\w+)", command)
+            if groups is not None:
+                # program name is first match
+                prog_name = groups.group(0)
+                # clean up duplicated white spaces
+                prog_name = ' '.join(prog_name.split())
+                # filter exceptions
+                if prog_name.lower() not in exceptions:
                     if prog_name not in deps:
                         deps[prog_name] = 1
                     else:
@@ -147,22 +145,26 @@ def main(argv=None):
     # add common options (-h/--help, ...) and parse command line
     (options, args) = E.Start(parser, argv=argv)
 
-    # check pipeline dependencies
+    # get dependencies dependencies
     deps, check_path_failures = checkDepedencies(options.pipeline)
 
-    # print dictionary ordered by value
-    if options.summary:
-        for k in sorted(deps, key=deps.get, reverse=True):
-            print('\nProgram: {0!s} used {1} time(s)'.format(k, deps[k]))
-
-    n_failures = len(check_path_failures)
-    if n_failures == 0:
-        print('\nCongratulations! All required programs are available on your PATH\n')
+    # print info about dependencies
+    if len(deps) == 0:
+        print('\nNo dependencies found.\n')
     else:
-        print('\nThe following programs are not on your PATH')
-        for p in check_path_failures:
-            print('\n{0!s}'.format(p))
-        print
+        # print dictionary ordered by value
+        if options.summary:
+            for k in sorted(deps, key=deps.get, reverse=True):
+                print('\nProgram: {0!s} used {1} time(s)'.format(k, deps[k]))
+
+        n_failures = len(check_path_failures)
+        if n_failures == 0:
+            print('\nCongratulations! All required programs are available on your PATH\n')
+        else:
+            print('\nThe following programs are not on your PATH')
+            for p in check_path_failures:
+                print('\n{0!s}'.format(p))
+            print
 
     # write footer and output benchmark information.
     E.Stop()
