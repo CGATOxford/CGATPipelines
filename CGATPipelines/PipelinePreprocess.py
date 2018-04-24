be34f69ecab3d3a6b79b1c113d56730a53569f6d
Alice Mayer <31933289+amayer21@users.noreply.github.com>
2018-04-24 11:17:47 +0100

    fix cutadapt paired untrimmed command (#423)

--- CGATPipelines/PipelinePreprocess.py
+++ CGATPipelines/PipelinePreprocess.py
@@ -546,8 +546,8 @@ class Cutadapt(ProcessTool):
 
             if untrimmed:
                 processing_options += \
-                    "--untrimmed-output=%(untrimmed_output1)s" \
-                    "--untrimmed-output-paired=%(untrimmed_output2)s" % locals()
+                    " --untrimmed-output=%(untrimmed_output1)s" \
+                    " --untrimmed-paired-output=%(untrimmed_output2)s" % locals()
 
             cmds.append('''
             cutadapt %(processing_options)s %(in1)s %(in2)s
